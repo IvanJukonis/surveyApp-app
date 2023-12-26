39d9f5df57d4e4c358af2d3146aeb885c1de6d13
unknown <ivanjukonis@hotmail.com>
2023-12-26 15:24:33 -0300

    SA-FE: DATEINPUT component styles corrections

--- src/Components/Shared/Inputs/DateInput/index.jsx
+++ src/Components/Shared/Inputs/DateInput/index.jsx
@@ -4,15 +4,17 @@ import styles from './date-input.module.css';
 const DateInput = ({ register, error, type, isDisabled, nameInput, nameTitle }) => {
   return (
     <div className={styles.wrapper}>
-      <label className={styles.dateLabel}>{nameTitle}</label>
-      <input
-        {...register(nameInput, { required: { value: true, message: 'This field is required' } })}
-        className={error ? `${styles.errorInput} ${styles.normalInput}` : styles.normalInput}
-        type={type}
-        disabled={isDisabled}
-        name={nameInput}
-      />
-      {error ? <p className={styles.error}>{error}</p> : <p className={styles.spaceErrorMsg}></p>}
+      <div className={styles.inputData}>
+        <input
+          {...register(nameInput, { required: { value: true, message: 'This field is required' } })}
+          className={error ? `${styles.errorInput} ${styles.normalInput}` : styles.normalInput}
+          type={type}
+          disabled={isDisabled}
+          name={nameInput}
+        />
+        <label className={styles.dateLabel}>{nameTitle}</label>
+        {error ? <p className={styles.error}>{error}</p> : <p className={styles.spaceErrorMsg}></p>}
+      </div>
     </div>
   );
 };
