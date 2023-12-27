3b719387a546e121c77b000d22a8bf7a803c47e6
unknown <ivanjukonis@hotmail.com>
2023-12-27 14:58:59 -0300

    SA-FE: Textarea input corrections

--- src/Components/Shared/Inputs/TextAreaInput/index.jsx
+++ src/Components/Shared/Inputs/TextAreaInput/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import styles from './normal-input.module.css';
+import styles from './textarea-input.module.css';
 
 const TextArea = ({ register, error, type, isDisabled, nameInput, nameTitle, styleInput }) => {
   return (
-    <div className={styles.nameLabel}>
-      <label>{nameTitle}</label>
-      <textarea
-        {...register(nameInput, { required: { value: true, message: 'This field is required' } })}
-        className={error ? `${styles.errorInput} ${styles[styleInput]}` : `${styles[styleInput]}`}
-        type={type}
-        disabled={isDisabled}
-        name={nameInput}
-      />
+    <div className={styles.wrapper}>
+      <div className={styles.inputData}>
+        <label className={styles.inputLabel}>{nameTitle}</label>
+        <textarea
+          {...register(nameInput, { required: { value: true, message: 'This field is required' } })}
+          className={error ? `${styles.errorInput} ${styles[styleInput]}` : `${styles[styleInput]}`}
+          type={type}
+          disabled={isDisabled}
+          name={nameInput}
+        />
+        <div className={styles.underline}></div>
+      </div>
       {error ? <p className={styles.error}>{error}</p> : <p className={styles.spaceErrorMsg}></p>}
     </div>
   );
