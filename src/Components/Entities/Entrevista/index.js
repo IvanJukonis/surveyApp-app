715090957fcf17af69342407a8c978c8a1398bcc
unknown <ivanjukonis@hotmail.com>
2024-01-08 13:03:12 -0300

    SA-FE: Agregar button from Entrevista component redirection functionality

--- src/Components/Entities/Entrevista/index.js
+++ src/Components/Entities/Entrevista/index.js
@@ -10,7 +10,7 @@ import {
 } from 'redux/entrevistaRoboRueda/thunks';
 import { Button, OptionInput } from 'Components/Shared';
 import { ToastError, TableComponent, Loader } from 'Components/Shared';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import styles from './entrevista.module.css';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
@@ -18,6 +18,7 @@ import Joi from 'joi';
 
 function Entrevista() {
   const dispatch = useDispatch();
+  const id = useParams();
   const entrevistaRoboRueda = useSelector((state) => state.entrevistaRoboRueda.list);
   const entrevistaSiniestro = useSelector((state) => state.entrevistaSiniestro.list);
   const isPending = useSelector((state) => state.siniestro.pending);
@@ -37,6 +38,7 @@ function Entrevista() {
   const actualUser = ['/relevador/siniestros', '/controlador/siniestros'].includes(
     location.pathname
   );
+  console.log(id);
 
   const schema = Joi.object({
     rol: Joi.string()
@@ -47,9 +49,7 @@ function Entrevista() {
     tipo: Joi.string().valid('Relevamiento', 'Fraude').messages({
       'any.only': 'Selecciona un tipo de siniestro permitido'
     }),
-    exportacion: Joi.string().valid('PDF', 'Word').messages({
-      'any.only': 'Selecciona un tipo de exportacion permitida'
-    })
+    exportacion: Joi.any()
   });
 
   const {
@@ -71,11 +71,24 @@ function Entrevista() {
     return '/administrativo';
   };
 
+  const handleAddClick = (rol, tipo) => {
+    const pathPrefix = getPathPrefix();
+    if (tipo == 'Fraude') {
+      history.push(`${pathPrefix}/siniestros/entrevistaroborueda/${rol}/${id.id}`);
+    } else {
+      history.push(`${pathPrefix}/siniestros/entrevistasiniestro/${rol}/${id.id}`);
+    }
+  };
+
   const handleEditClick = (item) => {
     const pathPrefix = getPathPrefix();
     history.push(`${pathPrefix}/siniestros/form/${item._id}`, {
       params: { ...item, mode: 'edit' }
     });
+  };
+
+  const onSubmit = (data) => {
+    handleAddClick(data.rol, data.tipo);
   };
 
   const deleteEntrevista = () => {
@@ -88,6 +101,8 @@ function Entrevista() {
     getAllEntrevistaSiniestro(dispatch);
     getAllEntrevistaRoboRueda(dispatch);
   }, []);
+
+  console.log(errors);
 
   return (
     <section className={styles.container}>
@@ -107,12 +122,12 @@ function Entrevista() {
         </div>
         <div className={styles.rightContainer}>
           <div className={styles.topContainer}>
-            <form className={styles.form} onSubmit={handleSubmit()}>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
               <div className={styles.inputContainer}>
                 <OptionInput
                   data={rolArray}
                   dataLabel="Seleccionar Rol"
-                  name="Rol"
+                  name="rol"
                   register={register}
                   error={errors.rol?.message}
                 />
@@ -121,7 +136,7 @@ function Entrevista() {
                 <OptionInput
                   data={tipoArray}
                   dataLabel="Seleccionar Tipo"
-                  name="Tipo"
+                  name="tipo"
                   register={register}
                   error={errors.tipo?.message}
                 />
@@ -133,7 +148,7 @@ function Entrevista() {
                 <OptionInput
                   data={tipoExportacion}
                   dataLabel="Exportaciones"
-                  name="Exportacion"
+                  name="exportacion"
                   register={register}
                   error={errors.exportacion?.message}
                 />
